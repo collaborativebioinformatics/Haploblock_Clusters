31a57859416a98a34679152b9613d8a3f878d736
Umran Yaman <72675757+umranyaman@users.noreply.github.com>
2024-08-30 21:42:29 +0100

    Update MHC_test_case_example.sh

--- scripts/MHC_test_case_example.sh
+++ scripts/MHC_test_case_example.sh
@@ -6,25 +6,26 @@
 # Obtain input_data.vcf for MHC. Example - HG002_MHCdiploidassembly_GRCh38_v1.0.cf.gz file from https://github.com/NCBI-Hackathons/TheHumanPangenome/tree/master/MHC
 
 # Define input files and parameters
-VCF_FILE="input_data.vcf"
-MHC_REGION="chr6:29677984-33485152"  # Common coordinates for MHC region (GRCh38)
-MHC_OUTPUT="mhc_haplotypes"
-ARG_OUTPUT="arg_output"
+VCF_FILE="input_data.vcf.gz"
+MHC_REGION="6:28510020-33480577"  # Common coordinates for MHC region (GRCh38)
+MHC_OUTPUT="mhc_haplotypes.vcf.gz"
+ARG_OUTPUT="arg_output.arg"
 
-# Step 1: Extract MHC haplotype blocks using plink
+# Step 1: Extract MHC haplotype blocks using bcftools
 echo "Extracting MHC haplotypes..."
-plink --vcf $VCF_FILE --chr 6 --from-bp 29677984 --to-bp 33485152 --recode vcf --out $MHC_OUTPUT
+bcftools view -r $MHC_REGION -Oz -o $MHC_OUTPUT $VCF_FILE
+bcftools index $MHC_OUTPUT
 
 # Check if the extraction was successful
 if [ $? -ne 0 ]; then
-    echo "Error extracting MHC haplotypes with plink."
+    echo "Error extracting MHC haplotypes with bcftools."
     exit 1
 fi
 
 # Step 2: Infer ARG using argweaver
 echo "Inferring ARG..."
 # arg-sample is used for ARG inference, here we assume the data is phased.
-arg-sample -n 10 -N 10000 -L 10000 -o $ARG_OUTPUT --vcf $MHC_OUTPUT.vcf
+arg-sample -n 10 -N 10000 -L 10000 -o $ARG_OUTPUT --vcf $MHC_OUTPUT
 
 # Check if ARG inference was successful
 if [ $? -ne 0 ]; then
@@ -36,8 +37,8 @@ fi
 # This step requires a custom analysis script or tool specific to your dataset
 # Here, we simply output the paths for further analysis
 echo "Linking MHC haplotype blocks with ARG data..."
-echo "MHC haplotype data: $MHC_OUTPUT.vcf"
-echo "ARG data: $ARG_OUTPUT.arg"
+echo "MHC haplotype data: $MHC_OUTPUT"
+echo "ARG data: $ARG_OUTPUT"
 
 # Optional: Use Python or R script to perform detailed linking and analysis
 # python link_haplotypes_with_arg.py --haplotypes $MHC_OUTPUT.vcf --arg $ARG_OUTPUT.arg
